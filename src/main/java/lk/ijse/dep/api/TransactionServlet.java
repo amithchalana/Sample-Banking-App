c78cd651c5b052f94a58a3474a880ee63e4abdc2
chalana <amithchalana19@gmail.com>
2023-01-08 18:32:12 +0530

    Deposit money webService completion

--- src/main/java/lk/ijse/dep/api/TransactionServlet.java
+++ src/main/java/lk/ijse/dep/api/TransactionServlet.java
@@ -11,12 +11,16 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep.dto.AccountDTO;
 import lk.ijse.dep.dto.TransactionDTO;
 import lk.ijse.dep.dto.TransferDTO;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.StringReader;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Date;
 
 
 @WebServlet(name = "transaction-servlet", urlPatterns = "/transactions/*" ,loadOnStartup = 0)
@@ -65,6 +69,7 @@ public class TransactionServlet extends HttpServlet {
     private void withdrawMoney(TransactionDTO transactionDTO, HttpServletResponse resp) {
 
 
+
     }
 
     private void transferMoney(TransferDTO transferDTO, HttpServletResponse resp) {
@@ -72,7 +77,66 @@ public class TransactionServlet extends HttpServlet {
 
     }
 
-    private void depositMoney(TransactionDTO transactionDTO, HttpServletResponse response) {
+    private void depositMoney(TransactionDTO transactionDTO, HttpServletResponse response) throws IOException {
+
+
+        try {
+            if (transactionDTO.getAccount() == null || !transactionDTO.getAccount().matches("[A-Fa-f0-9]{8}(-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12}")) {
+                throw new JsonException("Invalid account number");
+            } else if (transactionDTO.getAmount() == null || transactionDTO.getAmount().compareTo(new BigDecimal(100)) < 100) {
+                throw new JsonException("Invalid amount");
+            }
+            Connection connection = pool.getConnection();
+            PreparedStatement stm = connection.prepareStatement("SELECT  * FROM Account WHERE  account_number = ?");
+            stm.setString(1,transactionDTO.getAccount());
+            ResultSet rst = stm.executeQuery();
+            if (!rst.next()) {
+                throw new JsonException("Invalid account  number");
+            }
+
+            try {
+                connection.setAutoCommit(false);
+                PreparedStatement stmUpdate = connection.prepareStatement("UPDATE  Account SET balance = balance + ?  WHERE account_number = ?");
+                stmUpdate.setBigDecimal(1,transactionDTO.getAmount());
+                stmUpdate.setString(2,transactionDTO.getAccount());
+                if (stmUpdate.executeUpdate() != 1) {
+                    throw new SQLException("Failed to update the balance");
+                }
+
+                PreparedStatement stmNewTransaction = connection.prepareStatement("INSERT INTO Transaction  (account,type, description,amount ,date) VALUES (?,?,?,?,?)");
+                stmNewTransaction.setString(1,transactionDTO.getAccount());
+                stmNewTransaction.setString(2,"CREDIT");
+                stmNewTransaction.setString(3,"Deposit");
+                stmNewTransaction.setBigDecimal(4,transactionDTO.getAmount());
+                stmNewTransaction.setTimestamp(5,new Timestamp(new Date().getTime()));
+                if (stmNewTransaction.executeUpdate() != 1) {
+                    throw new SQLException("Failed to add a transaction record");
+                }
+                connection.commit();
+
+                ResultSet resultSet = stm.executeQuery();
+                resultSet.next();
+                String name = resultSet.getString("holder_name");
+                String address = resultSet.getString("holder_address");
+                BigDecimal balance = resultSet.getBigDecimal("balance");
+
+                AccountDTO accountDTO = new AccountDTO(transactionDTO.getAccount(), name, address, balance);
+                response.setStatus(HttpServletResponse.SC_CREATED);
+                response.setContentType("application/json");
+                JsonbBuilder.create().toJson(accountDTO, response.getWriter());
+            } catch (Throwable t) {
+                connection.rollback();
+                t.printStackTrace();
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Failed to deposit the money, contact the bank");
+            }finally {
+                connection.setAutoCommit(true);
+            }
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
 
     }
 }
